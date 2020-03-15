18d091d265f23da66055dfea11121091f7a4d75c
Namito Yokota <namitoyokota@Namitos-MacBook-Air.local>
2020-03-15 13:56:26 -0500

    formatted with prettier

--- src/helper.c
+++ src/helper.c
@@ -1,6 +1,7 @@
 #include "../lib/helper.h"
 
-int optionO() {
+int optionO()
+{
   int option;
   printf("\nSelect the Parallel Sieve of Eratosthenes algorithm to use:\n");
   printf("\t1. Default\n\t2. No even numbers\n\t3. Local computations (no broadcasts)\n\t4. Effective use of cache\n\t5. Quit\n\n");
--- src/sieve.c
+++ src/sieve.c
@@ -5,20 +5,25 @@ int main(int argc, char *argv[])
   int o;
   long n;
   while (1)
-	{
-		o = optionO();
-		if (o == 5)
-			return 0;
-		else if (o < 1 || o > 4)
-			continue;
-		else
+  {
+    o = optionO();
+    if (o == 5)
+      return 0;
+    else if (o < 1 || o > 4)
+      continue;
+    else
     {
       n = optionN();
-      if (o == 1) sieve1(argc, argv, n);
-      else if (o == 2) sieve2(argc, argv, n);
-      else if (o == 3) sieve3(argc, argv, n);
-      else if (o == 4) sieve4(argc, argv, n);
-      else printf("this shouldn't happen");
+      if (o == 1)
+        sieve1(argc, argv, n);
+      else if (o == 2)
+        sieve2(argc, argv, n);
+      else if (o == 3)
+        sieve3(argc, argv, n);
+      else if (o == 4)
+        sieve4(argc, argv, n);
+      else
+        printf("this shouldn't happen");
     }
   }
   return 0;
