119ba24383b0eaefdcc6f9d8ca564de8c096daef
Namito Yokota <namitoyokota@Namitos-MacBook-Air.local>
2020-03-15 13:14:02 -0500

    orgranized files structure!

---
+++ src/sieve.c
@@ -0,0 +1,11 @@
+#include "../lib/sieve.h"
+#include "../lib/sieve1.h"
+#include "../lib/sieve2.h"
+#include "../lib/sieve3.h"
+#include "../lib/sieve4.h"
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
